a119b9e37f003711012038cd841ad706c9bd37ae
Luis Filipe <exnlesi@gmail.com>
2014-01-09 19:50:25 +0000

    sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
- def sign_in(user)
+  def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
@@ -9,6 +9,13 @@ module SessionsHelper
 
   def signed_in?
     !current_user.nil?
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 
    def current_user=(user)
